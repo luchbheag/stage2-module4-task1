b9a792ce800eac2dd9a6f8d00c6c52caae348f6f
luchbheag <nedolivko.julia@gmail.com>
2024-07-29 16:47:29 +0200

    implement Singleton, Factory and Builder patterns

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,29 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        try (InputStream stream = H2ConnectionFactory.class
+                .getClassLoader()
+                .getResourceAsStream("h2database.properties")) {
+            Properties properties = new Properties();
+            properties.load(stream);
+            return DriverManager.getConnection(properties.getProperty("db_url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password"));
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
